acc55cf3faa463726627dc5f7ad45d33a3a79bd4
aadilahmed <aadilahmed0@gmail.com>
2018-04-26 17:56:15 -0700

    Initial Commit

--- app/src/main/java/com/udacity/sandwichclub/MainActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/MainActivity.java
@@ -10,6 +10,8 @@ import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Starting the project!!!!!
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
