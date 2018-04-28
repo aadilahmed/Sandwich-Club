2879c717293b3f14b8ded4de53a86a720a90b8b3
aadilahmed <aadilahmed0@gmail.com>
2018-04-27 19:44:14 -0700

    parseSandwichJson method complete

--- app/src/main/java/com/udacity/sandwichclub/MainActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/MainActivity.java
@@ -10,8 +10,6 @@ import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
-    //!Pokemon THING!!!!
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,35 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
-        return null;
+    public static Sandwich parseSandwichJson(String json) throws JSONException {
+        JSONObject sandwichDetails = new JSONObject(json);
+
+        JSONObject name = sandwichDetails.getJSONObject("name");
+        String mainName = name.getString("mainName");
+
+        JSONArray akaArray = name.getJSONArray("alsoKnownAs");
+        List<String> alsoKnownAs = new ArrayList<>();
+        for(int i = 0; i < akaArray.length(); i++) {
+            alsoKnownAs.add(akaArray.getString(i));
+        }
+
+        String placeOfOrigin = sandwichDetails.getString("placeOfOrigin");
+        String description = sandwichDetails.getString("description");
+        String image = sandwichDetails.getString("image");
+
+        JSONArray ingredientArray = sandwichDetails.getJSONArray("ingredients");
+        List<String> ingredients = new ArrayList<>();
+        for(int i = 0; i < ingredientArray.length(); i++) {
+            ingredients.add(ingredientArray.getString(i));
+        }
+
+        return new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
     }
 }
