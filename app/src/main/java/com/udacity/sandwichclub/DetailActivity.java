d7771e3ebc7e10e904201ccdef4aeddee3f8865a
aadilahmed <aadilahmed0@gmail.com>
2018-04-28 17:23:47 -0700

    populateUI built

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,11 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -36,19 +42,26 @@ public class DetailActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
-        if (sandwich == null) {
-            // Sandwich data unavailable
-            closeOnError();
-            return;
+        Sandwich sandwich = null;
+
+        try {
+            sandwich = JsonUtils.parseSandwichJson(json);
+
+            if (sandwich == null) {
+                // Sandwich data unavailable
+                closeOnError();
+                return;
+            }
+
+            populateUI(sandwich);
+            Picasso.with(this)
+                    .load(sandwich.getImage())
+                    .into(ingredientsIv);
+
+            setTitle(sandwich.getMainName());
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
-
-        populateUI();
-        Picasso.with(this)
-                .load(sandwich.getImage())
-                .into(ingredientsIv);
-
-        setTitle(sandwich.getMainName());
     }
 
     private void closeOnError() {
@@ -56,7 +69,33 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        TextView originTv = findViewById(R.id.origin_tv);
+        TextView descriptionTv = findViewById(R.id.description_tv);
+        TextView ingredientsTv = findViewById(R.id.ingredients_tv);
+        TextView alsoKnownTv = findViewById(R.id.also_known_tv);
 
+        descriptionTv.setText(sandwich.getDescription());
+        originTv.setText(sandwich.getPlaceOfOrigin());
+
+        List<String> akaArray = sandwich.getAlsoKnownAs();
+
+        for(int i = 0; i < akaArray.size(); i++) {
+            alsoKnownTv.append(akaArray.get(i));
+
+            if(i < akaArray.size() - 1) {
+                alsoKnownTv.append(", ");
+            }
+        }
+
+        List<String> ingredientsArray = sandwich.getIngredients();
+
+        for(int i = 0; i < ingredientsArray.size(); i++) {
+            ingredientsTv.append(ingredientsArray.get(i));
+
+            if(i < ingredientsArray.size() - 1) {
+                ingredientsTv.append(", ");
+            }
+        }
     }
 }
